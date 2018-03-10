da2b92e1fcd8c2251cd9ba7940be90054fd84ad1
bilalyldz <bilalyildizusa@gmail.com>
2018-03-10 13:55:13 -0600

    Yeay working

--- ThreeGit/src/one/One.java
+++ ThreeGit/src/one/One.java
@@ -3,7 +3,8 @@ package one;
 public class One {
 	public static void main(String[] args) {
 		System.out.println("Class One");
-		
-		System.out.println("bosh orib ketti:)");
+	System.out.println("bosh orib ketti:)");	
+	System.out.println("What it that man? :}");
+	
 	}
 }
